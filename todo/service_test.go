36953f366c755c7b1ed1c8dfdb926db4a56605cf
ethos93 <ethos93@gmail.com>
2018-06-28 17:42:28 +0900

    commit

---
+++ todo/service_test.go
@@ -0,0 +1,23 @@
+package todo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestService_Add(t *testing.T) {
+	instance := &service{}
+	orgLen := len(todos)
+	instance.Add("whatever")
+	assert.Equal(t, orgLen+1, len(todos))
+	assert.Equal(t, "whatever", todos[0])
+}
+
+func TestService_Add_Today(t *testing.T) {
+	instance := &service{}
+	orgLen := len(todoMap["2018-06-28"])
+	instance.Add("breakfast")
+	instance.Add("lunch")
+	assert.Equal(t, orgLen+2, len(todoMap["2018-06-28"]))
+}
